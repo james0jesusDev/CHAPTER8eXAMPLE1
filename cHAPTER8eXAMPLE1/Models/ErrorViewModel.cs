ff17cd1e8dac221ce70dc79d27195d9ebdb386cb
james0jesusDev <james0jesus@gmail.com>
2024-03-11 20:05:51 +0100

    Agregar archivos de proyecto.

---
+++ cHAPTER8eXAMPLE1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cHAPTER8eXAMPLE1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
